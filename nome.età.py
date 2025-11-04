60df9788d54ff1210fb6515b98fd2076d5f20a44
Guido.DeCarolis <guidolorenzodecarolis@liceoeinsteinmilano.edu.it>
2025-11-04 13:04:40 +0100

    finito programma di selezione del nome in base all'età

--- nome.età.py
+++ nome.età.py
@@ -1,9 +1,13 @@
 # nome ed età
 nome = input("inserisci il tuo nome:")
 età = int(input("inserisci la tua erà:"))
-if(età % 3 == 0) and (età % 2 != 0):
-    n1 = nome[:2] + nome[-2:]
-    print(n1)
+l = len (nome)
+if(l>4):
+    if(età % 3 == 0) and (età % 2 != 0):
+        n1 = nome[:2] + nome[-2:]
+        print(n1)
+    else:
+        n2 = nome[2:-2]
+        print(n2)
 else:
-    n2 = nome[2:-2]
-    print(n2)
+    print("il nome è troppo corto")
