56e52b1b9224d67aa9a8eaca22845aa7354036fd
Guido.DeCarolis <guidolorenzodecarolis@liceoeinsteinmilano.edu.it>
2025-11-04 12:44:04 +0100

    finito programma selezione in base all'età

---
+++ nome.età.py
@@ -0,0 +1,8 @@
+# nome ed età
+nome = input("inserisci il tuo nome:")
+età = int(input("inserisci la tua erà:"))
+if(età % 3 == 0) and (età % 2 != 0):
+    n1 = nome[:2] + nome[-2:]
+    print(n1)
+else:
+    n2 = nome[2:-2]
