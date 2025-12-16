fc39dedc3c2e0a34c389c30938dcfcea5d31d373
Guido.DeCarolis <guidolorenzodecarolis@liceoeinsteinmilano.edu.it>
2025-12-16 11:15:41 +0100

    finito programma divisori

--- Esercizio_divisori_con_cicli.py
+++ Esercizio_divisori_con_cicli.py
@@ -1,6 +1,6 @@
 #esercizio divisori
 numero = int(input("inserisci il numero: "))
-divisore = 2
+divisore = 1
 while divisore < numero // 2 or divisore == numero // 2:
     if(numero%divisore==0):
         print(divisore)
